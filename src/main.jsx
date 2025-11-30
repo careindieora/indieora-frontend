29c82ab4a4e776020e0bfcb86dbe02bba4754af4
smsvedang <sonivedang7@gmail.com>
2025-11-30 19:43:18 +0530

    update all

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,20 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './styles.css';
+import './styles.css'
 import App from './App'
 import Home from './pages/Home'
-import Shop from "./pages/Shop";
-import ProductDetail from "./pages/ProductDetail";
+import Shop from './pages/Shop'
+import ProductDetail from './pages/ProductDetail'
 
-// Admin pages
-import Login from '../../pages/admin/Login';
+// Admin pages (paths must be relative to src/)
+import Login from './pages/admin/Login'            // <-- fixed path
 import AdminDashboard from './pages/admin/Dashboard'
-import AdminList from './pages/admin/List'         // optional: product list page
-import AdminSettings from './pages/admin/Settings';
-import ProductList from './pages/admin/ProductList';
-import ProductForm from './pages/admin/ProductForm';
-import Categories from './pages/admin/Categories';
+import AdminList from './pages/admin/List'         // optional (remove if file doesn't exist)
+import AdminSettings from './pages/admin/Settings'
+import ProductList from './pages/admin/ProductList'
+import ProductForm from './pages/admin/ProductForm'
+import Categories from './pages/admin/Categories'
 
 // Admin layout wrapper
 import AdminLayout from './components/admin/AdminLayout'
@@ -35,16 +35,16 @@ createRoot(document.getElementById('root')).render(
         </Route>
 
         {/* Admin area (uses AdminLayout) */}
-      <Route path="/admin" element={<AdminLayout/>}>
-      <Route index element={<AdminDashboard/>} />
-      <Route path="products" element={<ProductList/>} />
-      <Route path="new" element={<ProductForm/>} />
-      <Route path="edit/:id" element={<ProductForm/>} />
-      <Route path="categories" element={<Categories/>} />
-      <Route path="settings" element={<AdminSettings/>} />
-      </Route>
+        <Route path="/admin" element={<AdminLayout/>}>
+          <Route index element={<AdminDashboard/>} />
+          <Route path="products" element={<ProductList/>} />
+          <Route path="new" element={<ProductForm/>} />
+          <Route path="edit/:id" element={<ProductForm/>} />
+          <Route path="categories" element={<Categories/>} />
+          <Route path="settings" element={<AdminSettings/>} />
+        </Route>
 
-        {/* You can add a 404 route here if desired */}
+        {/* 404 route could go here */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

8c8f7067326a1400f53fae33654ccc244a99ef97
smsvedang <sonivedang7@gmail.com>
2025-12-02 11:01:46 +0530

    step 8

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./app.jsx";
+import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import Shop from "./pages/Shop.jsx";
 import ProductDetail from "./pages/ProductDetail.jsx";
